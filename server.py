5046d2bb1bdf93931e2d7de1dd5ab6ab1fb1d65b
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-07-10 16:28:04 +0300

    add parser from itdtv

--- server.py
+++ server.py
@@ -3,6 +3,24 @@ import random
 from http import HTTPStatus
 
 
+def parse_http_response(text_response):
+    lines = text_response.split('\n')
+    status_raw, lines = lines[0], lines[1:]
+    protocol, status_code, message = status_raw.split(' ')
+    empty_index = 1
+    headers = {}
+    for index, line in enumerate(lines):
+        line = line.strip()
+        line = line.strip('\r')
+        if line == '':
+            empty_index = index
+            break
+        print(line)
+        k, _, v = line.partition(':')
+        headers.setdefault(k.strip(), v.strip())
+    content = ''.join(lines[empty_index + 1])
+    return int(status_code), headers, content
+
 
 HOST = "127.0.0.1"
 PORT = random.randint(10000, 20000)
@@ -33,8 +51,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         status_valuse = HTTPStatus.OK.value
         status_phrase = HTTPStatus.OK.phrase
 
-    massage = f"HTTP/1.1 200 OK\n Content-Length: 100\n Connection: close\n Content-Type: text/html\n\n <h1>Request Method: {request_method} " \
-              f"Request Source: {addres_port} Response Status: {status_valuse} {status_phrase} {headers_pretty}</h1>".encode("utf-8")
+
+    status_code, headers, content = parse_http_response(data.decode())
+
+    massage = f"HTTP/1.1 200 OK\n Content-Length: 100\n Connection: close\n Content-Type: text/html\n\n <h1>Status Code {status_code} Headers: {headers}, " \
+              f"Content {content}</h1>".encode("utf-8")
 
     connect.send(massage)
 
