aead92080c05fd7f2215fa6f43e9725369f53abb
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-07-10 12:14:04 +0300

    hw done

--- lesson/config.py
+++
@@ -1,8 +0,0 @@
-import random
-
-LOCALHOST = "127.0.0.1"
-
-
-def random_port():
-    """возвращает рандомный порт"""
-    return random.randint(20000, 30000)
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import socket
 import random
+from http import HTTPStatus
+
+
 
 HOST = "127.0.0.1"
 PORT = random.randint(10000, 20000)
 addres_port = (HOST, PORT)
-with socket.socket() as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(addres_port)  # биндим порт
 
     print(f'open adress and port -- curl {HOST}:{PORT}')  # сообщение о адресе и порте
@@ -16,12 +19,22 @@ with socket.socket() as s:
 
     request_method = data.decode('utf-8').split()[0]
     headers = data.decode('utf-8').split('\r\n')[8:]
+    headers_pretty = ''
+    for i in headers:
+        headers_pretty = headers_pretty + i + '\n'
 
-
-    response_status = 200
+    if b"status=404" in data:
+        status_valuse = HTTPStatus.NOT_FOUND.value
+        status_phrase = HTTPStatus.NOT_FOUND.phrase
+    elif b"status=200" in data:
+        status_valuse = HTTPStatus.OK.value
+        status_phrase = HTTPStatus.OK.phrase
+    else:
+        status_valuse = HTTPStatus.OK.value
+        status_phrase = HTTPStatus.OK.phrase
 
     massage = f"HTTP/1.1 200 OK\n Content-Length: 100\n Connection: close\n Content-Type: text/html\n\n <h1>Request Method: {request_method} " \
-              f"Request Source: {addres_port} Response Status: {response_status} OK {headers}</h1>".encode("utf-8")
+              f"Request Source: {addres_port} Response Status: {status_valuse} {status_phrase} {headers_pretty}</h1>".encode("utf-8")
 
     connect.send(massage)
 
